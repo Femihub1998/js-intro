537ce7cad42e1effdbf7ea497078bd7358fc2642
femi biolatiri <femibiolatiri987654321@gmail.com>
2022-06-23 00:34:06 +0100

    linked

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+let name, height, country;
+
+name = "Femi Biolatiri";
+height = "5'7 feet";
+country = "Nigeria";
+
+console.log(`Name: ${name}`);
+console.log(`Height: ${height}`);
+console.log(`Country: ${country}`);
+alert(`Name: ${name} \n Height: ${height}  \n Country: ${country}.`);
